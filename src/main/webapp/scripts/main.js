8b9b6875b60fb8128ede832cc9ea24b906315735
jinblo <114090577+jinblo@users.noreply.github.com>
2023-04-10 13:59:00 +0300

    Initial upload

---
+++ src/main/webapp/scripts/main.js
@@ -0,0 +1,23 @@
+function haeAsiakkaat() {
+	let url = "asiakkaat";
+	let requestOptions = {
+			method: "GET",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" }
+	};
+	fetch(url, requestOptions)
+	.then(response => response.json())
+	.then(response => printItems(response))
+	.catch(errorText => console.error("Fetch failed: " + errorText));
+}
+function printItems(respObjList) {
+	let htmlStr="";
+	for (let item of respObjList) {
+		htmlStr = htmlStr+ "<tr id='rivi_" + item.asiakas_id + "'>";
+		htmlStr = htmlStr+ "<td>" + item.etunimi + "</td>";
+		htmlStr = htmlStr+ "<td>" + item.sukunimi + "</td>";
+		htmlStr = htmlStr+ "<td>" + item.puhelin + "</td>";
+		htmlStr = htmlStr+ "<td>" + item.sposti + "</td>";
+		htmlStr = htmlStr+ "</tr>";
+	}
+	document.getElementById("tbody").innerHTML = htmlStr;
+}
